4c71330cbe3ac09fad8e642ea005b36c3ea280e3
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-11-13 12:17:20 -0500

    style: Eslint/Prettier Config

--- src/Utils/Generate.ts
+++ src/Utils/Generate.ts
@@ -1,9 +1,9 @@
-//generate random string func
+// generate random string func
 export function generateRandomString(length: number): string {
-    let text = "";
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { fastify } from "fastify";
-import fastifyAutoload from "fastify-autoload";
-import { join } from "path";
+import {fastify} from 'fastify';
+import fastifyAutoload from 'fastify-autoload';
+import {join} from 'path';
 const server = fastify();
 
 server.register(fastifyAutoload, {
-  dir: join(__dirname, "Routes"),
-  options: { prefix: "/" },
-})
+  dir: join(__dirname, 'Routes'),
+  options: {prefix: '/'},
+});
 
-server.listen(process.env.PORT ?? 3000, "0.0.0.0", async (err, address) => {
+server.listen(process.env.PORT ?? 3000, '0.0.0.0', async (err, address) => {
   if (err) {
     throw err;
   }
