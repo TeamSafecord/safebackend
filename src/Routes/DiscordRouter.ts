dce55da4171098f07a684496ecc6e0b319098b62
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-11-25 17:21:56 -0500

    refactor: Use Math.Round > ToFixed
    Doing this solely for speed purposes, I would've never committed this otherwise
    
    Co-authored-by: Nick <42541753+Nciklol@users.noreply.github.com>

--- src/Routes/DiscordRouter.ts
+++ src/Routes/DiscordRouter.ts
@@ -79,7 +79,7 @@ export default async (server: FastifyInstance) => {
       nonce,
       JSON.stringify(newSession),
       'EX',
-      ((Date.now() + 1000 * 60 * 60 * 2) / 1000).toFixed(0),
+Math.round(((Date.now() + 1000 * 60 * 60 * 2) / 1000)),
     );
 
     reply.setCookie('access', nonce, {
